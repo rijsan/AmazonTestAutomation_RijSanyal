434132b081fb36bd33a5b9a8817e84f43db15bf2
Rijul Sanyal <rijsanyal@deloitte.com>
2024-09-05 00:52:13 +0530

    Adding the common and frequently used Page Objects and Xpaths in BasePage.java

--- src/main/java/com/Amazon/AmazonTestAutomation/pages/BasePage.java
+++ src/main/java/com/Amazon/AmazonTestAutomation/pages/BasePage.java
@@ -1,6 +1,50 @@
 package com.Amazon.AmazonTestAutomation.pages;
 
+import com.Amazon.AmazonTestAutomation.utils.LoggerUtil;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class BasePage
 {
+    public WebDriver driver;
+    public WebDriverWait wait;
+    public static LoggerUtil logger;
+
+
+    public BasePage(WebDriver driver)
+    {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver,this);
+        logger = new LoggerUtil();
+    }
+
+    @FindBy(xpath = "//input[@id='add-to-cart-button']")
+    private WebElement AddToCartButton;
+
+    @FindBy(xpath = "//a[@id='nav-cart']")
+    private WebElement NavigationToCart;
+
+    @FindBy(xpath = "//span[@id='productTitle']")
+    private WebElement ProductTitle;
+
+
+    public void Click_AddToCartButton() {
+        AddToCartButton.click();
+        logger.info("Add To Cart Button is Clicked");
+    }
+    public void OpenCartLink() {
+        NavigationToCart.click();
+        logger.info("Navigation to the Cart screen is clicked");
+    }
+    public String GetProductTitleText() {
+        return ProductTitle.getText();
+    }
+
 
 }
